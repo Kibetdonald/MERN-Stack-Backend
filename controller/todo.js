12cc5dd91320543fdf7583d24ee62ba58d24a527
Don <kibetdonald97@gmail.com>
2022-07-17 19:57:35 +0300

    Worked on post and get request

--- controller/todo.js
+++ controller/todo.js
@@ -1,15 +1,11 @@
+// Get the todo model
 const Todo = require("../model/todoModel");
 
-exports.createProduct = (req, res) => {
+// Create Products
+exports.createTodo = (req, res) => {
   const { who, slug, task, isDone, created_at, updatedAt } = req.body;
 
-  if (req.files.length > 0) {
-    productPictures = req.files.map((file) => {
-      return { img: file.filename };
-    });
-  }
-
-  const product = new Product({
+  const todo = new Todo({
     who: req.body.who,
     slug: req.body.slug,
     task: req.body.task,
@@ -21,7 +17,35 @@ exports.createProduct = (req, res) => {
   todo.save((error, todo) => {
     if (error) return res.status(400).json({ error });
     if (todo) {
-      res.status(201).json({ todo});
+      res.status(201).json({ todo });
     }
   });
 };
+
+// Get products from the database
+exports.getTodo = async (req, res) => {
+  const todo = await Todo.find()
+    .select("_id who slug task isDone created_at")
+    // .populate({ path: "category", select: "_id name" })
+    .exec();
+
+  res.status(200).json({ todo });
+};
+
+// Delete todo item by id
+exports.deleteTodoById = (req, res) => {
+  const { todoId } = req.body.payload;
+  if (todoId) {
+    Todo.deleteOne({ _id: todoId }).exec((error, result) => {
+      if (error) return res.status(400).json({ error });
+      if (result) {
+        res.status(202).json({ result });
+      } 
+    });
+  } else {
+    res.status(400).json({ error: "Params required" });
+  }
+};
+
+
+
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,19 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const {
-  createTodo
-
+  createTodo,
+  getTodo,
+  deleteTodoById
 } = require("../controller/todo");
 
 // Post request
-router.post("/todo/create");
-
+router.post(
+  "/todo/create",
+  createTodo
+);
 
 // Get request
+router.get(
+  "/todos",
+  getTodo
+ 
+);
+
 
 // Delete request
-
+router.delete(
+  "/todo/deleteTodoById/:productId",
+  deleteTodoById
+);
 // Update Request
+router.get("update/id", (req, res)=>{
+  res.send("Update request")
+})
 
-
+// exporting the router
 module.exports = router;
